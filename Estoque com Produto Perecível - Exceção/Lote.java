4f486717c249b9c40b7f30d99c20558461b37bcd
Nicole <127159893+Nicoleel336@users.noreply.github.com>
2024-02-19 10:13:33 -0300

    Create Lote.java

---
+++ Estoque com Produto Perecível - Exceção/Lote.java
@@ -0,0 +1,35 @@
+package nicoleEllen.estoqueComProdutoPerecivelExcecoes;
+import java.util.Date;
+
+/* quantidade de produto
+ * data de validade
+ * */
+
+public class Lote {
+	private int quantidade;
+	public Date val;
+	
+	//construtor
+	public Lote(int quant, Date validade) {
+		quantidade = quant;
+		val = validade;
+	}
+	
+	public Date getValidade() {
+		return val;
+	}
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public void setQuantidade(int quant) {
+		quantidade = quant;
+	}
+	
+	public void venda(int quant) {
+		quantidade -= quant;
+	}
+	
+	
+}
