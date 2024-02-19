1e7da17fb8dbb4d33c2ad708443848bd7be9fb8e
Nicole <127159893+Nicoleel336@users.noreply.github.com>
2024-02-19 10:16:27 -0300

    Create ProdutoInexistente.java

---
+++ Estoque com Produto Perecível - Exceção/ProdutoInexistente.java
@@ -0,0 +1,10 @@
+package nicoleEllen.estoqueComProdutoPerecivelExcecoes;
+
+public class ProdutoInexistente extends Exception {
+
+	//recebe o código do produto
+	public ProdutoInexistente() {
+		//super("O produto de número " + cod + " não existe.");
+	}
+	
+}
